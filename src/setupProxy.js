30d5c6af6b6c2f365f74dbdf0bb7f64078a782f8
dima_samborskyi <samborskiydmitriy1@gmail.com>
2022-08-20 16:20:58 +0300

    .

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: 'https://the-dark-api.herokuapp.com/',
       changeOrigin: true,
     })
   );
